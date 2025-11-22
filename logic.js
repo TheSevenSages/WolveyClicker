537d868314c82e8974aec9700deb80f548f78a1c
Jacob Robinson <Jacobrobinson4321@gmail.com>
2025-11-22 14:49:15 -0500

    Shop items do stuff now

--- logic.js
+++ logic.js
@@ -1,3 +1,28 @@
 let mdpCount = 0;
 let creditsPerClick = 1;
+let totalCreditsPerSecond = 0;
 
+let _itemTemplate = {
+    count: 0,
+    creditsPerSecond: 1
+}
+
+let itemTracker = [
+    JSON.parse(JSON.stringify(_itemTemplate)),
+    JSON.parse(JSON.stringify(_itemTemplate)),
+    JSON.parse(JSON.stringify(_itemTemplate)),
+    JSON.parse(JSON.stringify(_itemTemplate)),
+    JSON.parse(JSON.stringify(_itemTemplate))
+]
+
+function tick()
+{
+    let tickValue = 0
+    itemTracker.forEach(element => {
+        tickValue += element.count * (element.creditsPerSecond / 60)
+    });
+    mdpCount += tickValue
+    totalCreditsPerSecond = Math.floor(tickValue * 60)
+}
+// Set tick speed to 1/60th of a second
+window.setInterval(tick, 1000 / 60)
